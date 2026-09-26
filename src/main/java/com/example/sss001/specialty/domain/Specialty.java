17022a6ec31398c585ee6ecb25f65d4326d581b3
alvaro <davidalvaro026@gmail.com>
2026-09-25 22:15:23 -0500

    feat: add database constraints to Specialty and MedicalService

--- src/main/java/com/example/sss001/specialty/domain/Specialty.java
+++ src/main/java/com/example/sss001/specialty/domain/Specialty.java
@@ -14,6 +14,6 @@ public class Specialty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false, unique = true, length = 80)
     private String name;
 }
